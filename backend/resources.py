4cce3ba11e880a7244f6ba72fc24995b174b7e1e
Ragavardaan <ragavardaan.2006@gmail.com>
2026-03-16 19:01:50 +0530

    Update resources.py

--- backend/resources.py
+++ backend/resources.py
@@ -1,10 +1,11 @@
 import mysql.connector
 
 conn = mysql.connector.connect(
-    host="mysql-atlb.railway.internal",
-    user="root",
-    password="WimQnTTlRrXENFJqeJgfnQrsPRRkwexf",  
-    database="encryption_db"
+    host=os.environ.get("DB_HOST"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASS"),
+    database=os.environ.get("DB_NAME"),
+    port=int(os.environ.get("DB_PORT", 3306))
 )
 cursor = conn.cursor()
 
