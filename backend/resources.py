864e017a05c9d0903cbb6ca8a0d3ab7c2f0c2783
Ragavardaan <ragavardaan.2006@gmail.com>
2026-03-19 12:41:04 +0530

    Update resources.py

--- backend/resources.py
+++ backend/resources.py
@@ -1,14 +1,3 @@
-import mysql.connector
-
-conn = mysql.connector.connect(
-    host=os.environ.get("DB_HOST"),
-    user=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASS"),
-    database=os.environ.get("DB_NAME"),
-    port=int(os.environ.get("DB_PORT", 3306))
-)
-cursor = conn.cursor()
-
 def get_resources():
     return {
         "AES": "Symmetric key algorithm. Fast and secure for large data.",
